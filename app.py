c0232b00a3a2aff7be42b991eacea655b9f49df3
Sanmit Singh <97466473+sxnmit@users.noreply.github.com>
2024-01-18 17:48:10 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,58 @@
 import serial.tools.list_ports
 from flask import Flask, render_template, request,redirect,url_for
 from threading import Thread
+from flask_socketio import SocketIO
+from threading import Thread
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+app = Flask(__name__)
+app.debug = True
 
 # Create serial port instance
-serialInst = serial.Serial()
-port = '/dev/tty.usbmodem1201'
-serialInst.baudrate = 9600
-serialInst.port = port 
-serialInst.open()
+serialInst = serial.Serial('/dev/tty.usbserial-120', 9600)
+#port = '/dev/tty.usbmodem1201'
+#serialInst.port = port 
+#serialInst.open()
 
 # Global variable to store status
-x = 'Away'
+pk = 'Away'
+ss = "Away"
+sj = "Away"
+am = "Away"
 
 def check_access(data):
-    for letter in data:
-        if letter == "i":
-            return False
-    return True
+    global pk,ss,sj,am
+    if '1' in data:
+        pk="Home"
+    elif data == '2':
+        ss="Home"
+    elif data=='3':
+        sj="Home"
+    elif data=='4':
+        am="Home"
+    return pk,ss,sj,am
 
 def read_serial_data():
-    global x
-    while True:
-        if serialInst.in_waiting:
-            packet = serialInst.readline()
-            received_data = packet.decode('utf')
-            if check_access(received_data):
-                x = "Home"
+    global pk, ss, sj, am
+    try:
+        while True:
+            if serialInst.is_open:
+                if serialInst.in_waiting:
+                    packet = serialInst.readline()
+                    received_data = packet.decode('utf')
+                    pk, ss, sj, am = check_access(received_data)
+                    print(pk)
+                    socketio.emit('status_update', {'status1': pk, 'status2': ss, 'status3': sj, 'status4': am})
             else:
-                x = "Away"
+                print("Serial port is not open.")
+    except Exception as e:
+        print(f"Error in read_serial_data: {e}")
+            
+            
+            
 
 # Start the background thread to continuously check the status
 serial_thread = Thread(target=read_serial_data)
@@ -39,14 +62,6 @@ serial_thread.start()
 def home():
     return render_template("login.html")
 
-
-@app.route('/change_status/<roommate>/<new_status>')
-def change_status(roommate, new_status):
-    global roommate_statuses
-    roommate_statuses[roommate] = new_status
-    return redirect(url_for('index'))
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error_message = None
@@ -54,7 +69,7 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
         if username == "509" and password == "123":
-            return render_template("index.html",status=x)
+            return render_template("index.html",status1=pk,status2=ss,status3=sj,status4=am)
     
     return render_template('login.html', error_message=error_message)
 
