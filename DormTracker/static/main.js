c9bd3dab98cbf2bc2ff7a62c70046ea9ae2a69b1
Sanmit Singh <97466473+sxnmit@users.noreply.github.com>
2024-03-04 17:17:56 -0500

    Add files via upload

--- DormTracker/static/main.js
+++ DormTracker/static/main.js
@@ -1,3 +1,5 @@
+const socket = io.connect('http://192.168.2.57:5001/login');
+
 function status_change1() {
 
   const Home = document.createElement('button');
@@ -102,7 +104,6 @@ function status_change4() {
     Away.style.display = "none";
   });
 
-  const socket = io.connect('http://your-flask-server-address:5001');
 
   // Listen for the 'status_update' event
   socket.on('status_update', function (data) {
@@ -112,17 +113,11 @@ function status_change4() {
     const status3 = data.status3;
     const status4 = data.status4;
 
-    // Update your HTML or perform any other actions with the received status data
-    console.log('Received status update:', status1, status2, status3, status4);
-
     // Example: Update HTML elements
     document.getElementById('status1').innerText = status1;
     document.getElementById('status2').innerText = status2;
     document.getElementById('status3').innerText = status3;
     document.getElementById('status4').innerText = status4;
-
-    console.log('hi')
-    location.reload()
   });
 
 }
